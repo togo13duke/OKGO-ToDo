c8ba85f6219bc630fbe40a035bc5abf2ea6679a9
togo <togo13duke@gmail.com>
2023-05-10 00:45:08 +0900

    code format

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const h1Elm = document.querySelector('h1');
 
-    if (h1Elm){
-        h1Elm.addEventListener('click',function (){
-            if (h1Elm.style.color === 'blue'){
+    if (h1Elm) {
+        h1Elm.addEventListener('click', function () {
+            if (h1Elm.style.color === 'blue') {
                 h1Elm.style.color = 'red';
-            }else {
+            } else {
                 h1Elm.style.color = 'blue';
             }
         })
