cf85a5036e56fb6417d6ff2fc604ac5a04afd677
togo <togo13duke@gmail.com>
2023-05-10 00:34:52 +0900

    template

---
+++ main.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const h1Elm = document.querySelector('h1');
+
+    if (h1Elm){
+        h1Elm.addEventListener('click',function (){
+            if (h1Elm.style.color === 'blue'){
+                h1Elm.style.color = 'red';
+            }else {
+                h1Elm.style.color = 'blue';
+            }
+        })
+    }
+});
